4288e335a5b68346724fcdc168c453af2dd446de
Hikmat Jafarli <jafarlihi@gmail.com>
2022-11-11 02:08:39 +0400

    Add stuff

--- tests.c
+++ tests.c
@@ -7,6 +7,17 @@ int main(int argc, char *argv[]) {
   char grammarString[] = "S -> A A\nA -> a A | b\nB -> #";
   Grammar *grammar = parseGrammar(grammarString);
   printf("%s\n", getGrammarAsString(grammar));
+
+  char grammarString2[] = "Program -> var Variables begin Operators end\n"
+                          "Variables -> Variable ; Variables\n"
+                          "Variables -> #\n"
+                          "Variable -> identifier\n"
+                          "Operators -> Operator ; Operators\n"
+                          "Operators -> #\n"
+                          "Operator -> read ( Variable )\n"
+                          "Operator -> write ( Variable )\n";
+  grammar = parseGrammar(grammarString2);
+  printf("%s\n", getGrammarAsString(grammar));
 }
 
 #endif
